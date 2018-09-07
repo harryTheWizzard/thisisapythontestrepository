23fbdc1e8f9acd625200487c4039fcd7e0665d92
harryTheWizzard <43062552+harryTheWizzard@users.noreply.github.com>
2018-09-07 14:30:10 +0200

    added test class
    just adding some shit

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 #main python file
 #testing github
+class test: 
+    def __init__(self,id):
+        try:
+            self.id = id
+            self.name = "car" + str(id)
+        except ValueError:
+            print("did not work")
 for i in range(300):
     print("reeee\n\r")
 
